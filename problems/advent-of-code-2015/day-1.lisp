5f646f242041e79984b845250ba85d2d8e6081ee
Anderson Sacramento <anderson.sacramento93@gmail.com>
2022-04-14 19:59:12 -0300

    Add day 1 part 2 solution advent of code 2015

--- problems/advent-of-code-2015/day-1.lisp
+++ problems/advent-of-code-2015/day-1.lisp
@@ -1,16 +1,29 @@
-(defun day-1 (floor-cmds)
+(defun day-1-part-1 (floor-cmds)
   (loop for cmd across floor-cmds
         if (string= "(" cmd)
-          sum 1
+        sum 1
         else
-          when (string= ")" cmd)
-            sum -1
+        when (string= ")" cmd)
+        sum -1
         end))
 
 
-(defun day-1 (floor-cmds)
+(defun day-1-part-1 (floor-cmds)
   (reduce #'+ (map 'list #'(lambda (cmd)
                           (cond ((string= cmd "(") 1)
                                 ((string= cmd ")") -1)
                                 (t 0)))
                       floor-cmds)))
+
+
+(defun day-1-part-2 (floor-cmds)
+  (loop for cmd across floor-cmds
+        for i = 1 then (1+ i)
+        if (string= "(" cmd)
+            sum 1 into floor
+        else
+            when (string= ")" cmd)
+                sum -1 into floor
+        end
+        when (= floor -1)
+        return i))
