d1bc2e4922f08e5b7c9b61839cef797e5ba404b5
Anderson Sacramento <anderson.sacramento93@gmail.com>
2022-05-26 22:38:58 -0300

    Add advent of code 2015 day 6 part 2 solution and refactor part 1 code

--- problems/advent-of-code-2015/day-6.lisp
+++ problems/advent-of-code-2015/day-6.lisp
@@ -28,25 +28,41 @@
              collect (cons (pop positions)
                            (pop positions)))))))
 
+(defmacro x-pos (point)
+  `(car ,point))
+
+(defmacro y-pos (point)
+  `(cdr ,point))
+
+(defmacro aref-light (lights point)
+  `(aref ,lights (x-pos ,point) (y-pos ,point)))
+
 (defun turn-on (point lights)
-  (setf (aref lights (car point) (cdr point)) 1))
+  (setf (aref-light lights point) 1))
 
 (defun turn-off (point lights)
-  (setf (aref lights (car point) (cdr point)) 0))
+  (setf (aref-light lights point) 0))
+
+(defun inc-light (point lights &optional (delta 1))
+  (incf (aref-light lights point) delta))
+
+(defun dec-light (point lights &optional (delta 1))
+  (when (>= (aref-light lights point) 1)
+    (decf (aref-light lights point) delta)))
 
 (defun litp (point lights)
-  (not (zerop (aref lights (car point) (cdr point)))))
+  (not (zerop (aref lights (x-pos point) (y-pos point)))))
 
 (defun toggle (point lights)
   (if (litp point lights)
       (turn-off point lights)
       (turn-on point lights)))
 
-(defun count-lights-lit (lights)
+(defun count-lit-lights (lights)
   (loop for i from 0 below 1000
         sum (loop for j from 0 below 1000
                   when (litp (cons i j) lights)
-                  sum 1)))
+                  sum (aref-light lights (cons i j)))))
 
 (defun run-cmd (cmd-fn points lights)
   (mapc #'(lambda (point)
@@ -54,19 +70,37 @@
         points))
 
 (defun day-6-part-1 (instructions)
-  (let ((lights (make-array '(1000 1000) :element-type '(unsigned-byte 1))))
+  (lights-interpreter instructions))
+
+(defun day-6-part-2 (instructions)
+  (lights-interpreter instructions
+                      :turn-on-fn #'inc-light
+                      :turn-off-fn #'dec-light
+                      :toggle-fn #'(lambda (point lights)
+                                     (inc-light point
+                                                lights
+                                                2))))
+
+
+(defun lights-interpreter (instructions
+                           &key (turn-on-fn #'turn-on)
+                                (turn-off-fn #'turn-off)
+                                (toggle-fn #'toggle))
+  (let ((lights (make-array '(1000 1000)
+                            :element-type 'integer
+                            :initial-element 0)))
     (loop for instruction in (cl-ppcre:split #\NewLine instructions)
           do (let ((cmd (cmd-from instruction))
                    (points (multiple-value-bind (upper-left down-right)
                                (positions-from instruction)
                              (gen-points upper-left down-right))))
                (cond ((string= cmd "turn on")
-                      (run-cmd #'turn-on points lights))
+                      (run-cmd turn-on-fn points lights))
                      ((string= cmd "toggle")
-                      (run-cmd #'toggle points lights))
+                      (run-cmd toggle-fn points lights))
                      ((string= cmd "turn off")
-                      (run-cmd #'turn-off points lights)))))
-    (count-lights-lit lights)))
+                      (run-cmd turn-off-fn points lights)))))
+    (count-lit-lights lights)))
 
 
 
